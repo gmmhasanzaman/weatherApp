cdd23f3833b4538c5fee1a10411452b1205af185
Samuel Peña <samuel-andres.pena@necomplus.com>
2019-03-21 09:53:33 -0400

    Added the deserializer to the retrofit instance.

--- app/src/main/java/com/sampp/weatherapp/api/WeatherAppApi.java
+++ app/src/main/java/com/sampp/weatherapp/api/WeatherAppApi.java
@@ -1,5 +1,10 @@
 package com.sampp.weatherapp.api;
 
+
+import com.google.gson.GsonBuilder;
+import com.sampp.weatherapp.api.deserializers.CityDeserializer;
+import com.sampp.weatherapp.models.City;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -11,9 +16,15 @@ public class WeatherAppApi {
     // singleton design pattern to get the retrofit instance
     public static Retrofit getApi(){
         if(retrofit == null){
+
+            // Gson Builder to add the deserializer class to the retrofit builder.
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.registerTypeAdapter(City.class, new CityDeserializer());
+
+            // retrofit instance.
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(gsonBuilder.create()))
                     .build();
         }
         return retrofit;
