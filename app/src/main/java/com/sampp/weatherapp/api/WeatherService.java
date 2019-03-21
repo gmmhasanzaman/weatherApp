720450fe1dbe0d7a122c49c580f23606c5917c0c
samuelpena21 <samuelpena098@gmail.com>
2019-03-21 07:09:29 -0400

    add retrofit and begin with the initial structure

---
+++ app/src/main/java/com/sampp/weatherapp/api/WeatherService.java
@@ -0,0 +1,5 @@
+package com.sampp.weatherapp.api;
+
+public interface WeatherService {
+
+}
---
+++ app/src/main/java/com/sampp/weatherapp/models/City.java
@@ -0,0 +1,7 @@
+package com.sampp.weatherapp.models;
+
+public class City {
+    private String name;
+    private int lat;
+    private int lon;
+}
