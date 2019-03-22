8e9619abdf294f979b7f808f7ec6f5709fd4f478
samuelpena21 <samuelpena098@gmail.com>
2019-03-22 00:49:20 -0400

    Added the city name and the language configuration for the api. Setup the day/night mode

--- app/src/main/java/com/sampp/weatherapp/activities/MainActivity.java
+++ app/src/main/java/com/sampp/weatherapp/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.sampp.weatherapp.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,6 +13,8 @@ import com.sampp.weatherapp.api.services.WeatherService;
 import com.sampp.weatherapp.models.City;
 import com.squareup.picasso.Picasso;
 
+import java.util.Locale;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -20,15 +23,16 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String METRIC_UNIT = "Metric";
     ImageView weatherImg;
-    TextView weatherTemperatureTxt,weatherDescriptionTxt;
+    TextView weatherTemperatureTxt,weatherDescriptionTxt,weatherCityTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupDayNightMode();
         initializeVisuals();
         WeatherService service = WeatherAppApi.getApi().create(WeatherService.class);
 
-        Call<City> cityCall = service.getCityByName("Seville,ES", WeatherAppApi.KEY,METRIC_UNIT,"es");
+        Call<City> cityCall = service.getCityByName("Seville,ES", WeatherAppApi.KEY,METRIC_UNIT, Locale.getDefault().getLanguage());
 
         cityCall.enqueue(new Callback<City>() {
             @Override
@@ -46,10 +50,16 @@ public class MainActivity extends AppCompatActivity {
         weatherImg = findViewById(R.id.weather_img);
         weatherTemperatureTxt = findViewById(R.id.weather_temperature);
         weatherDescriptionTxt = findViewById(R.id.weather_description);
+        weatherCityTxt = findViewById(R.id.weather_city);
+    }
+
+    public void setupDayNightMode(){
+        getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
     }
 
     // update the UI with the result.
     public void setResult(City city){
+        weatherCityTxt.setText(city.getName());
         weatherTemperatureTxt.setText(String.valueOf(city.getTemperature()));
         weatherDescriptionTxt.setText(city.getDescription());
         Picasso.get().load(WeatherAppApi.BASE_ICONS_URL + city.getIcon() + WeatherAppApi.ICON_EXTENSION).into(weatherImg);
